30f58ce793e460ae34381c44f1fe49996c1143d4
P <sp83@LAMU02YQ2KPLVCJ.uhc.com>
2021-08-11 19:14:31 +0530

    added another method and validated time

--- linked_lists/singly_linked_list_cycles.py
+++ linked_lists/singly_linked_list_cycles.py
@@ -7,6 +7,9 @@ O(n) time and O(1) space.
 
 """
 
+import profile
+import timeit
+
 
 class Node(object):
 
@@ -27,3 +30,46 @@ def is_cycled(first_node):
             return True
 
     return False
+
+
+def is_cycled_v2(first_node):
+    if not first_node:
+        return "Node provided is null"
+
+    runner = first_node.next
+    while runner and runner is not first_node:
+        runner = runner.next
+
+    return runner == first_node
+
+
+r = Node(1)
+a = Node(2)
+b = Node(3)
+c = Node(4)
+d = Node(5)
+e = Node(6)
+r.next = a
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = r
+print(is_cycled(r))
+print(is_cycled_v2(r))
+print('\n\n')
+profile.run('is_cycled(r)')
+print('\n\n')
+profile.run('is_cycled_v2(r)')
+
+print('\n\n')
+test_code1 = '''
+def test():
+    return is_cycled(r)
+'''
+test_code2 = '''
+def test():
+    return is_cycled_v2(r)
+'''
+print(timeit.repeat(stmt=test_code1, repeat=5))
+print(timeit.repeat(stmt=test_code2, repeat=5))
